11e18a9f59d85c3c7463f49ec509311e29b5e4b3
Krzysztof Pająk <kpajak@gmail.com>
2015-07-09 15:01:07 +0200

    Project configuration

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./src/routes/index');
+var indexRouter = require('./src/routes/index');
+var config = require('nconf');
 
 var app = express();
 
@@ -13,7 +13,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'src/views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -21,20 +20,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+// attach routers
+app.use('/', indexRouter);
 
-// catch 404 and forward to error handler
+// 404
 app.use(function(req, res, next) {
+    console.log(app.get('env'));
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
+// dev error handler
+if ('dev' === config.get('NODE_ENV')) {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
@@ -44,8 +42,7 @@ if (app.get('env') === 'development') {
     });
 }
 
-// production error handler
-// no stacktraces leaked to user
+// error handler
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,32 @@
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
+var mocha = require('gulp-mocha');
+var istanbul = require('gulp-istanbul');
+var mocha = require('gulp-mocha');
+var tap = require('gulp-tap');
+var coverageReportDir = 'coverage-report';
+
 var jsSourceFiles = ['app.js', 'src/*/*.js'];
 var testFiles = ['test/*/*.test.js'];
 
 gulp.task('lint', function () {
-    var jshint = require('gulp-jshint');
-
     return gulp.src(jsSourceFiles)
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish'));
 });
 
 gulp.task('jscs', function () {
-    var jscs = require('gulp-jscs');
-
     return gulp.src(jsSourceFiles.concat(testFiles))
         .pipe(jscs());
 });
 
 gulp.task('mocha', function () {
-    var mocha = require('gulp-mocha');
-
     return gulp.src(testFiles, { read: false })
         .pipe(mocha());
 });
 
 gulp.task('istanbul', function (cb) {
-    var istanbul = require('gulp-istanbul');
-    var mocha = require('gulp-mocha');
-    var tap = require('gulp-tap');
-    var coverageReportDir = 'coverage-report';
-
     gulp.src(jsSourceFiles)
         .pipe(istanbul({ includeUntested: true }))
         .pipe(istanbul.hookRequire())
